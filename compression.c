b1446e9d5c3d26786a4fcd05a86cf1e6b0fa2283
Jason Cheng <jasoncheng224@gmail.com>
2023-11-21 20:16:11 -0800

    Do-nothing compression implementation

--- compression.c
+++ compression.c
@@ -1,1 +1,9 @@
 #include "compression.h"
+
+void compress(const char *in, size_t in_size, void (*write)(const char *, size_t)) {
+    write(in, in_size);
+}
+
+void decompress(const char *in, size_t in_size, void (*write)(const char *, size_t)) {
+    write(in, in_size);
+}
--- compression.h
+++ compression.h
@@ -4,10 +4,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-void compress(const char *in, size_t in_size,
-              void (*write)(const char *, size_t));
+void compress(const char *in, size_t in_size, void (*write)(const char *, size_t));
 
-void decompress(const char *in, size_t in_size,
-                void (*write)(const char *, size_t));
+void decompress(const char *in, size_t in_size, void (*write)(const char *, size_t));
 
 #endif
