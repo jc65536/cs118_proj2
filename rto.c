de71069ba6fad11e1567da79f8afa8f218084187
Jason Cheng <jasoncheng224@gmail.com>
2023-12-06 08:59:51 -0800

    Reinstate lb

--- rto.c
+++ rto.c
@@ -7,7 +7,9 @@
 #define A 0.125
 #define B 0.25
 
-struct timespec rto = (struct timespec){.tv_sec = 1, .tv_nsec = 0};
+#define RTO_LB 500000000
+
+struct timespec rto = (struct timespec){.tv_sec = 0, .tv_nsec = RTO_LB};
 
 static int consecutive_doubling = 0;
 bool lossy_link = false;
@@ -31,10 +33,10 @@ void log_send(seqnum_t seqnum) {
     flag = true;
 }
 
-void log_ack(seqnum_t acknum) {
-    static uint64_t srtt;
-    static uint64_t rttvar;
+static uint64_t srtt;
+static uint64_t rttvar;
 
+void log_ack(seqnum_t acknum) {
     if (!flag || acknum <= stored_seqnum)
         return;
 
@@ -52,9 +54,9 @@ void log_ack(seqnum_t acknum) {
 
     uint64_t rto_ = srtt + 2 * rttvar;
 
-    if (rto_ < S_TO_NS) {
-        rto.tv_sec = 1;
-        rto.tv_nsec = 0;
+    if (rto_ < RTO_LB) {
+        rto.tv_sec = 0;
+        rto.tv_nsec = RTO_LB;
     } else {
         rto.tv_sec = rto_ / S_TO_NS;
         rto.tv_nsec = rto_ % S_TO_NS;
@@ -81,5 +83,6 @@ void double_rto() {
     }
 
     flag = false;
+    srtt = rttvar = 0;
     consecutive_doubling++;
 }
