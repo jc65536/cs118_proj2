af33ee93e384ccef06406931ef01621940b7db9c
Jason Cheng <jasoncheng224@gmail.com>
2023-12-06 03:31:23 -0800

    Correct holes

--- client-timer.c
+++ client-timer.c
@@ -4,8 +4,6 @@
 #include "rto.h"
 
 void handle_timer(union sigval args) {
-    // static const struct packet *last_retrans_packet;
-
     struct timer_args *targs = (struct timer_args *) args.sival_ptr;
     struct sendq *sendq = targs->sendq;
     struct retransq *retransq = targs->retransq;
@@ -15,25 +13,19 @@ void handle_timer(union sigval args) {
     printf("Timeout!!\n");
 #endif
 
-    // p is the oldest in-flight packet, or NULL if there are no in-flight
-    // packets.
-    // const struct packet *p = sendq_oldest_packet(sendq);
-
     if (holes_len) {
-        // retransq_push(retransq, p->seqnum);
-
-        for (size_t i = 0; i < holes_len; i++) {
+        for (size_t i = 0; i < holes_len; i++)
             retransq_push(retransq, holes[i]);
-        }
 
         holes_len = 0;
+    } else {
+        const struct packet *p = sendq_oldest_packet(sendq);
 
-        // last_retrans_packet = p;
+        if (p)
+            retransq_push(retransq, p->seqnum);
     }
 
-    // if (p != last_retrans_packet)
     sendq_halve_ssthresh(sendq);
-
     sendq_set_cwnd(sendq, 1);
     double_rto();
     set_timer(timer);
